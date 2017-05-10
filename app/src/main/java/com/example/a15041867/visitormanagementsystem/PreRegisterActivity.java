add025329c9bb0f01a5e5ce6b23c538056b37212
15039840 <15039840@myrp.edu.sg>
2017-05-10 19:05:34 +0800

    change button cancel to option menu

--- app/src/main/java/com/example/a15041867/visitormanagementsystem/PreRegisterActivity.java
+++ app/src/main/java/com/example/a15041867/visitormanagementsystem/PreRegisterActivity.java
@@ -2,6 +2,9 @@ package com.example.a15041867.visitormanagementsystem;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,7 +15,7 @@ import android.widget.EditText;
 public class PreRegisterActivity extends AppCompatActivity{
 
     EditText etNRIC, etTextName, etNumber, etEmail;
-    Button btnSubmit, btnCancel;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,21 @@ public class PreRegisterActivity extends AppCompatActivity{
         etNRIC = (EditText)findViewById(R.id.editTextNRIC);
         etNumber = (EditText)findViewById(R.id.editTextPhoneNumber);
         etTextName = (EditText)findViewById(R.id.editTextName);
-        btnCancel = (Button)findViewById(R.id.buttonCancel);
         btnSubmit = (Button)findViewById(R.id.buttonSubmit);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.action_cancel) {
+            return true;
+        }
+        return false;
     }
 }
