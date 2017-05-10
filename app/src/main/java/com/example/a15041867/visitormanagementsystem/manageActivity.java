5129f44fe4676a6ae96e21e9dde4eea225d1466b
15017082 <15017082@myrp.edu.sg>
2017-05-10 14:50:26 +0800

    Juble

---
+++ app/src/main/java/com/example/a15041867/visitormanagementsystem/manageActivity.java
@@ -0,0 +1,13 @@
+package com.example.a15041867.visitormanagementsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class manageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage);
+    }
+}
