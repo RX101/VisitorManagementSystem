b8a9c2d6de521deaef2f5dc92235164c732ef940
15017082 <15017082@myrp.edu.sg>
2017-05-16 00:01:23 +0800

    Juble

--- app/src/main/java/com/example/a15041867/visitormanagementsystem/ManagerActivity.java
+++ app/src/main/java/com/example/a15041867/visitormanagementsystem/ManagerActivity.java
@@ -1,13 +1,59 @@
 package com.example.a15041867.visitormanagementsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ManagerActivity extends AppCompatActivity {
+
+    Button btnInfo, btnSummary, btnEvacuation, btnManage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
+
+        btnInfo = (Button)findViewById(R.id.btnInfo);
+        btnSummary = (Button)findViewById(R.id.btnSummary);
+        btnEvacuation = (Button)findViewById(R.id.btnEvacuation);
+        btnManage = (Button)findViewById(R.id.btnManage);
+
+        btnInfo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent i = new Intent(ManagerActivity.this, userInfoActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnSummary.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent i = new Intent(ManagerActivity.this, summaryActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnEvacuation.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent i = new Intent(ManagerActivity.this, evacuationActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnManage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent i = new Intent(ManagerActivity.this, manageActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
